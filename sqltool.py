f2169557d159a2c2d3178033bcaa53ab7d39e453
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2023-12-03 06:35:33 +0000

    Cast first creature spell without stack. Next do the stack

--- sqltool.py
+++ sqltool.py
@@ -1,7 +1,10 @@
 import sqlite3
 import random
+import re
+import activateAbilities
 
 con = sqlite3.connect("mtgopen.db")
+con.row_factory = sqlite3.Row
 db = con.cursor()
 
 def GetLibraryIDs(gameID, playerIndex):
@@ -20,10 +23,58 @@ def GetHandIDs(gameID, playerIndex):
         handList.append(card[0])
     return handList
 
+def GetBattlefieldIDs(gameID, playerIndex):
+    data = [gameID, playerIndex]
+    battlefield = (db.execute("SELECT id FROM ingamecards WHERE game=? AND location='battlefield' AND owner=?;", data)).fetchall()
+    battlefieldList = []
+    for card in battlefield:
+        battlefieldList.append(card[0])
+    return battlefieldList
+
+def GetManaSources(gameID, playerIndex):
+    data = [gameID, playerIndex]
+    manaSources = (db.execute("SELECT id FROM ingamecards JOIN cards ON ingamecards.printedid=cards.Nid WHERE game=? AND location='battlefield' AND owner=? AND Ngenerated_mana IS NOT NULL;", data)).fetchall()
+    manaSourcesList = []
+    for card in manaSources:
+        manaSourcesList.append(card[0])
+    return manaSourcesList
+
+def UndoTempTaps(id):
+    print("insert UndoTempTaps code here")
+    return    
+
 def IngameCardIDToName(id):
     data = [id]
     name = (db.execute("SELECT Nname FROM cards JOIN ingamecards ON cards.Nid = ingamecards.printedid WHERE ingamecards.id=?;", data)).fetchone()[0]
     return name
+
+def IngameCardIDToObject(id):
+    data = [id]
+    cardObject = (db.execute("SELECT * FROM cards JOIN ingamecards ON cards.Nid = ingamecards.printedid WHERE ingamecards.id=?;", data)).fetchall()[0]
+    return cardObject
+
+def IngameCardIDToCostString(id):
+    data = [id]
+    costString = (db.execute("SELECT Nmana_cost FROM cards JOIN ingamecards ON cards.Nid = ingamecards.printedid WHERE ingamecards.id=?;", data)).fetchone()[0]
+    return costString
+def TappedCheck(cardIndex):
+    data = [cardIndex]
+    if (db.execute("SELECT tapped FROM ingamecards WHERE id=?", data)).fetchone()[0] ==0:
+        return False
+    else:
+        return True
+
+def TapLand(id, playerIndex, destID):
+    if activateAbilities.CheckForActivatedAbility(id, playerIndex, destID):
+        return True
+    else:
+        return False
+            
+def UntapLand(id, playerIndex):
+    if activateAbilities.CheckForActivatedAbility(id, playerIndex):
+        return True
+    else:
+        return False
     
 def StartingLife(gameID, format, players):
     # set life
@@ -70,20 +121,45 @@ def PrintHands(gameID, playerIndex):
 def Action(gameID, playerIndex):
     while True:
         try:
-            answer = str(input(f"{playerNames[playerIndex]}: you have priority:\nH: Play a card from your Hand.\nP: Pass Priority.\nWhat would you like to do?\n"))
+            answer = str(input(f"{playerNames[playerIndex]}: you have priority:\nH: Play a card from your Hand.\nB: Interact with a card on the Battlefield.\nP: Pass Priority.\nWhat would you like to do?\n"))
         except ValueError:
             print("Invalid response: please enter a letter")
             continue
         if answer == 'p':
             break
         elif answer == "h":
-            print("play a card from hand")
             PlayCardHand(playerIndex)
+            continue
+        elif answer == "b":
+            BattlefieldInteraction(playerIndex)
             continue
         else:
             print("Invalid response: please choose one of the options")
             continue
 
+def BattlefieldInteraction(playerIndex):
+    # get hand int a list
+    battlefield = GetBattlefieldIDs(gameID, playerIndex)
+    while True:
+        print(f"Pick a card: ")
+        for y in range(len(battlefield)):
+            name = IngameCardIDToName(battlefield[y])
+            if TappedCheck(battlefield[y]):
+                print(f"{(y + 1)}: {name} (Tapped)")
+            else:
+                print(f"{(y + 1)}: {name} (Untapped)")
+        print("Or P to cancel.")
+        answer = input(f"Pick an option:")
+        if answer == 'p':
+            break
+        chosenInGameCardID = battlefield[int(answer) - 1]
+        if ActivateAbility(chosenInGameCardID, playerIndex):
+            break
+        else:
+            print("Nothing happened, please pick again")
+            continue
+        return
+        
 def PlayCardHand(playerIndex):
     # get hand int a list
     hand = GetHandIDs(gameID, playerIndex)
@@ -100,33 +176,175 @@ def PlayCardHand(playerIndex):
         if PlayCard(chosenInGameCardID, playerIndex):
             break
         else:
-            print("Card cannot be played, please pick again")
+            print("Card was not played, please pick again")
             continue
         
+def ActivateAbility(chosenInGameCardID, playerIndex):
+    if activateAbilities.CheckForActivatedAbility(chosenInGameCardID, playerIndex):
+        return True
+    else:
+        return False              
+
 def PlayCard(id, playerIndex):
     # get card type
     # ADD COLUMN TO INGAMECARDS CALLED TYPE WHERE YOU CAN CHECK IF ANY TYPE CHANGES HAVE OCCURED CHECK THAT BEFORE YOU RUN THE PRINTED CARDS TYPE, IF NOT RUN THE PRINTED CARDS TYPE
     data = [id]
     cardType = (db.execute("SELECT Ntype from cards JOIN ingamecards ON cards.Nid=ingamecards.printedid WHERE ingamecards.id=?", data)).fetchone()[0]
-    print(cardType)
     if "Land" in cardType:
-        print("Card is a Land")
         data = [gameID]
-        land = (db.execute("SELECT land" + str(playerIndex) + " FROM games WHERE id=?", data)).fetchone()[0]
+        land = (db.execute("SELECT land FROM games WHERE id=?", data)).fetchone()[0]
         if land > 0:
             # change land location from hand to battlefield and tapped to 0
             data = [id]
-            db.execute("UPDATE ingamecards SET location='battlefield', tapped=0 WHERE id=?", data)
+            db.execute("UPDATE ingamecards SET location='battlefield', tapped=0, facedown=0 WHERE id=?", data)
             land -= 1
             data = [land, gameID]
-            db.execute("UPDATE games SET land" + str(activePlayer) + " = ? WHERE id=?;", data)
+            db.execute("UPDATE games SET land = ? WHERE id=?;", data)
             con.commit()
             print("Card played")
             return True
         else:
             print("You cannot play anymore lands this turn.")
             return False
+    elif "Creature" in cardType:
+        if HasMana(id, playerIndex):
+            # CastSpell(id)
+            data = [id]
+            db.execute("UPDATE ingamecards SET location='battlefield', tapped=0, facedown=0 WHERE id=?", data)
+            return True
+        else:
+            if PayCardCost(id, playerIndex):
+                # CastSpell(id)
+                data = [id]
+                db.execute("UPDATE ingamecards SET location='battlefield', tapped=0, facedown=0 WHERE id=?", data)
+                return True
+            else:
+                return False
     
+def HasMana(id, playerIndex):
+    
+    
+    # manaSources = GetManaSources(gameID, playerIndex)
+    while True:
+        
+        data = [gameID, playerIndex, id]
+        manaPoolDB = (db.execute("SELECT mana FROM mana_pool WHERE game_id=? AND player_index=? AND (dest_ingamecard_id IS NULL OR dest_ingamecard_id=?) AND (spent IS NULL OR spent=0)", data)).fetchall()
+
+        name = IngameCardIDToName(id)
+        manaString = IngameCardIDToCostString(id)
+        print("This is the mana pool:")
+        print(manaPoolDB)
+        # Compare price to pool
+        while True:
+            for x in range(len(manaPoolDB)):
+                print(manaString[len(manaString) - 3:])
+                if manaString[len(manaString) - 3:] in manaPoolDB[x]:
+                    manaString = re.sub(manaString[len(manaString) - 3:],'',manaString)
+                    manaPoolDB.pop(x)
+                    if not manaString:
+                        print("debug10")
+                        return True
+                    continue
+            break
+        
+        manaSources = GetManaSources(gameID, playerIndex)
+        print(f"To cast {name} you need to pay {manaString}\nTap lands for mana:")
+        for y in range(len(manaSources)):
+            cardObject = IngameCardIDToObject(manaSources[y])
+            if cardObject['tapped'] == 0:
+                print(f"{(y + 1)}: {cardObject['Nname']} (Untapped)")
+            else:
+                print(f"{(y + 1)}: {cardObject['Nname']} (Tapped)")
+        print("Or C to cancel.")
+        answer = input(f"Pick an option:")
+        if answer == 'c':
+            UndoTempTaps(id)
+            break
+        chosenInGameCardID = manaSources[int(answer) - 1]
+        if TapLand(chosenInGameCardID, playerIndex, id):
+            continue
+        else:
+            print("Card was not played, please pick again")
+            continue
+        
+    print("erm")
+"""
+    # this needs to be a loop through tuples
+    canCast = False
+    availableMana = GetAvailableMana(playerIndex)
+    castCost = GetCastCost(id)
+    while True:
+        if availableMana['SW'] > 0:
+            if castCost['SW'] > 0:
+                castCost.update({'SW':(castCost['SW'] - availableMana['SW'])})
+
+        if GetCastCost(id) <= GetAvailableMana(playerIndex):
+            return True
+        else:
+            return False
+"""        
+def GetCastCost(id):
+    castCosts = {'W':0, 'U':0, 'B':0, 'R':0, 'G':0, '?':0,'UB':0, 'BR':0,'RG':0,'GW':0,'WB':0,'UR':0,'BG':0,'RW':0,'GU':0,'2W':0,'2U':0,'2B':0,'2R':0,'2G':0,'PW':0, 'PU':0, 'PB':0, 'PR':0, 'PG':0, 'PGU':0, 'PRG':0, 'PRW':0, 'PGW':0, 'S':0}
+    xInstances = 0
+    data = [id]
+    castCostsString = (db.execute("SELECT Nmana_cost FROM cards JOIN ingamecards ON cards.Nid=ingamecards.printedid WHERE ingamecards.id=?", data)).fetchone()[0]
+    for x in range(len(castCostsString)):
+        if castCostsString[x] == '{':
+            if castCostsString[x + 1] == 'W' and castCostsString[x + 2] == '}':
+                castCosts.update({'W':(castCosts['W'] + 1)})
+            elif castCostsString[x + 1] == 'U' and castCostsString[x + 2] == '}':
+                castCosts.update({'U':(castCosts['U'] + 1)})
+            elif castCostsString[x + 1] == 'B' and castCostsString[x + 2] == '}':
+                castCosts.update({'B':(castCosts['B'] + 1)})
+            elif castCostsString[x + 1] == 'R' and castCostsString[x + 2] == '}':
+                castCosts.update({'R':(castCosts['R'] + 1)})
+            elif castCostsString[x + 1] == 'G' and castCostsString[x + 2] == '}':
+                castCosts.update({'G':(castCosts['G'] + 1)})
+            elif castCostsString[x + 1] == 'X' and castCostsString[x + 2] == '}':
+                xInstances += 1
+            elif castCostsString[x + 1].isdigit() and castCostsString[x + 2] == '}':
+                castCosts.update({'?':(castCosts['?'] + int(castCostsString[x + 1]))})
+    if xInstances > 0:
+        xValue = GetXCastValue(id)
+        xCost = xValue * xInstances
+        castCosts.update({'?':(castCosts['?'] + xCost)})
+    print(castCosts)
+    return castCosts
+
+def GetXCastValue(id):
+    cardName = IngameCardIDToName(id)
+    while True:
+        try:
+            xCastValue = int(input(f"X value for {cardName}?"))
+        except ValueError:
+            print("Invalid response: please enter a number")
+            continue
+        if xCastValue < 0:
+            print("Invalid response: please enter a value greater than 0")
+            continue
+        else:
+            break
+    return xCastValue
+        
+def GetAvailableMana(playerIndex):
+    manaPoolDict = {'W':0, 'U':0, 'B':0, 'R':0, 'G':0, '?':0, 'SW':0, 'SU':0, 'SB':0, 'SR':0, 'SG':0, 'S?':0}
+    data = [gameID,playerIndex]
+    manaPoolDB = (db.execute("SELECT mana FROM mana WHERE game_id=? AND player_index=?", data)).fetchall()
+    for item in manaPoolDB:
+        print(item)
+    if manaPoolDB is not None:
+        for x in range(len(manaPoolDB)):
+            if 'W' in manaPoolDB[x]:
+                manaPoolDict.update({'W':(manaPoolDict['W'])})
+
+            # NOT WORKIUNG
+            # SOME MAGIC TO EXTRACT A READABLE MANA DICT
+        
+
+        print("Thar be Mana")
+    else:
+        print("no mana in pool")
+    return manaPoolDict
         
 def PriorityLoop(players):
     data = [gameID]
@@ -173,12 +391,11 @@ def ActivePriority():
             break
         
 
-    
-def DrainManaPools(gameID):
+def DrainManaPools(gameID, phaseInt):
     for x in range(6):
-        data = [gameID]
-        db.execute("UPDATE games SET colorless_mana" + str(x) + "=0,white_mana" + str(x) + "=0,blue_mana" + str(x) + "=0,black_mana" + str(x) + "=0,red_mana" + str(x) + "=0,green_mana" + str(x) + "=0 WHERE id=?;", data)
-    con.commit()
+        data = [phaseInt, gameID, phaseInt]
+        db.execute("UPDATE mana_pool SET dest_event_id=? WHERE game_id=? AND (dest_ingamecard_id IS null OR dest_ingamecard_id = '') AND (dest_event_id IS null OR dest_event_id = '') AND ((mana_drain IS null OR mana_drain = '') OR mana_drain=?)", data)
+        con.commit()
     
 def Mulligan(gameID, players):
     # 2 dicts for mulligan decisions/reprecusions
@@ -196,10 +413,10 @@ def Mulligan(gameID, players):
         for x in range(len(players)):
             if playerMulliganDecision[x] == True and playerReady[x] == False:
                                 # add code to show if other player(s) is(are) ready
-                if x == startingPlayer:
+                if x == startingPlayerIndex:
                     print("You go first")
                 else:
-                    print("Your oppontent goes first")
+                    print("Your opponent goes first")
 
                 print(playerNames[x] + "'s draw:")
                 data = [x, gameID]
@@ -258,7 +475,7 @@ def Mulligan(gameID, players):
 def BeginningPhase(gameID):
     # MAKE THIS A FUNCTION AT SOME POINT
     data = [gameID]
-    db.execute("UPDATE games SET land" + str(activePlayer) + " = 1 WHERE id=?;", data)
+    db.execute("UPDATE games SET land= 1 WHERE id=?;", data)
     con.commit()
 
     UntapPhase(gameID)
@@ -300,7 +517,7 @@ def UntapPhase(gameID):
     #drain mana from pools
     data = [gameID]
     db.execute("UPDATE games SET phase=4 WHERE id=?;", data)
-    DrainManaPools(gameID)
+    DrainManaPools(gameID,4)
 
 def UpkeepPhase(gameID):
     data = [gameID]
@@ -315,7 +532,7 @@ def UpkeepPhase(gameID):
     #drain mana from pools
     data = [gameID]
     db.execute("UPDATE games SET phase=7 WHERE id=?;", data)
-    DrainManaPools(gameID)
+    DrainManaPools(gameID,7)
 
 def DrawPhase(gameID):
     # active player draws
@@ -330,7 +547,7 @@ def DrawPhase(gameID):
     #drain mana from pools
     data = [gameID]
     db.execute("UPDATE games SET phase=10 WHERE id=?;", data)
-    DrainManaPools(gameID)
+    DrainManaPools(gameID,10)
 
 monoWhiteDeck = ["631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "631590", "636994", "636994", "636994", "636994","534751", "534751", "534751",
 "534751", "534760", "534760", "534760", "534760", "534781", "534781", "534781", 
